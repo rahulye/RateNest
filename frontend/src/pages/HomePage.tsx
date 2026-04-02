d57ff62576355eeca64f03fee588d4b9fd96c495
Rahul-krishh <rahulmykris@gmail.com>
2026-04-02 20:46:23 +0530

    chore: deployment node is tweaking

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -80,7 +80,7 @@ const HomePage = () => {
 			) : (
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 					{products!.data!.map((product) => (
-						<ProductCard key={product.id} product={product} />
+						<ProductCard product={product} />
 					))}
 				</div>
 			)}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,12 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	server:{
-		port:5173,
-		open:true  // allow to poen in our browser
-	}
+  plugins: [react()],
+  server: {
+    port: 5173,
+    open: true, // opens in the browser
+  },
 });
